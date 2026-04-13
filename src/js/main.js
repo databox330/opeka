b3e675ba23ef80bee5d8b079c5265c8796d2f9dc
Kilros0817 <lazarkitfrl@gmail.com>
2026-04-13 12:48:48 -0700

    refine structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,20 @@
 import './assets-vars.js'
 import '../styles/main.scss'
+
+import '../components/button/button.scss'
+import '../components/cta-excursion/cta-excursion.scss'
+import '../components/logo/logo.scss'
+import '../components/pager/pager.scss'
+import '../components/promo-card/promo-card.scss'
+import '../components/scroll-hint/scroll-hint.scss'
+import '../components/tag/tag.scss'
+import '../components/text-link/text-link.scss'
+
+import '../sections/site-header/site-header.scss'
+import '../sections/hero/hero.scss'
+import '../sections/main-section/main-section.scss'
+import '../sections/site-footer/site-footer.scss'
+
 import { initHero } from './modules/scenes/hero.js'
 
 initHero()
